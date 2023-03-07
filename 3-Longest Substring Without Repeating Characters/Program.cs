5f805345f33de3a7c0d25c74ac2a7ef82bcd4cd6
Blake Sanders <blake.sanders@stentam.com>
2023-03-07 13:03:11 -0500

    3-longest substring without repeating characters

---
+++ 3-Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        string sub = "";
+        int longest = 0;
+        int index = 1;
+        for (int i = 0; i < s.Length; i++)
+        {
+            Console.WriteLine(sub);
+            if (!sub.Contains(s[i]))
+            {
+                sub = sub + s[i];
+                if (sub.Length == 1)
+                {
+                    index = i;
+                }
+            }
+            else {
+                if (longest < sub.Length)
+                {
+                    longest = sub.Length;
+                    Console.WriteLine($"longest: {longest} => string: {sub}");
+                }
+                sub = "";
+                Console.WriteLine($"Start Index = {index} => char: {s[index]} || Curent Index: {i}");
+                i = index;
+            }
+        }
+
+        if (longest < sub.Length)
+        {
+            longest = sub.Length;
+        }
+
+        return longest;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution i = new Solution();
+        var test = i.LengthOfLongestSubstring("dvdf");
+        Console.WriteLine(test.ToString());
+    }
+}
+
